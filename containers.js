db0288129f5e622fd9b1584f5d01c15b23d3e306
CoralCastillo <158122504+CoralCastillo@users.noreply.github.com>
2024-02-26 08:30:42 -0800

    Update containers.js

--- containers.js
+++ containers.js
@@ -1,15 +1,21 @@
-// Crear y agregar 10 contenedores al div "contenedores"
-for (let i = 0; i < 10; i++) { // aqu
-    // Crear un div contenedor
-    const container = document.createElement('div');
-    container.style.height = '100px'; // Tamaño del contenedor
-    
-    // Generar un color aleatorio hexadecimal
-    const colores = '#' + Math.floor(Math.random()*16777215).toString(16);
-    
-    // Asignar el color aleatorio al contenedor
-    container.style.backgroundColor = colores;
-    
-    // Agregar el contenedor al div con id "contenedores"
-    document.getElementById('containers').appendChild(container);
+const body = document.querySelector('body');
+let colores = ['pink', 'yellow', 'red', 'blue', "#9BC7F5", "#BB3CB6", "#BAF2F3", "AF9EFF", "#50BBAD",
+"#62BB50", "#FFB17A", "#7AF5FF", "#642F61", "#2838D7", "#CA0E61", "#2AECB4", "#830022", 
+"#0E4001", "#6A6A87", "#73A35F"];
+function generarColorUnico() {
+    let color;
+    do {
+        color = colores[Math.floor(Math.random() * colores.length)];
+    } while (coloresUtilizados.includes(color));
+    coloresUtilizados.push(color);
+    return color;
+}
+
+const coloresUtilizados = [];
+
+for (let i = 0; i < 20; i++) {
+    const div = document.createElement('div');
+    div.style.height = '100px';
+    div.style.backgroundColor = generarColorUnico();
+    body.appendChild(div);
 }
